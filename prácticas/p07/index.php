197252646fcef82b6d960ebb425143ec7b22bf79
Mau <mauricio.gonzalezt@alumno.buap.mx>
2024-09-10 14:02:08 -0600

    añadiendo src/funciones.php

--- prácticas/p07/index.php
+++ prácticas/p07/index.php
@@ -3,39 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Práctica 4</title>
+    <title>Práctica 7</title>
 </head>
 <body>
+    
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
     <?php
-        if(isset($_GET['numero']))
-        {
-            $num = $_GET['numero'];
-            if ($num%5==0 && $num%7==0)
-            {
-                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-            }
-            else
-            {
-                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-            }
-        }
-    ?>
+        // Incluir el archivo con las funciones
+        include 'src/funciones.php';
+        // Verificar si se ha pasado el número como parámetro en la URL
+        if (isset($_GET['numero'])) {
+            $numero = $_GET['numero'];
 
-    <h2>Ejemplo de POST</h2>
-    <form action="http://localhost/tecweb/practicas/p04/index.php" method="post">
-        Name: <input type="text" name="name"><br>
-        E-mail: <input type="text" name="email"><br>
-        <input type="submit">
-    </form>
-    <br>
-    <?php
-        if(isset($_POST["name"]) && isset($_POST["email"]))
-        {
-            echo $_POST["name"];
-            echo '<br>';
-            echo $_POST["email"];
+            // Validar si el valor es numérico
+            if (is_numeric($numero)) {
+                // Llamar a la función y mostrar el resultado
+                echo esMultiploDe5y7($numero);
+            } else {
+                echo "Por favor, ingresa un número válido.";
+            }
+        } else {
+            echo "No se ha proporcionado ningún número en la URL.";
         }
     ?>
 </body>
