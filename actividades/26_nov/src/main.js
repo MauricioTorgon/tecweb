2cebf89f1c2ca93996fe65bde9d55f33875735d5
Maurice <mauricio.gonzalezt@alumno.buap.mx>
2024-09-27 19:58:39 -0600

    añadiendo ejercicio 6 y 7

--- actividades/26_nov/src/main.js
+++ actividades/26_nov/src/main.js
@@ -26,6 +26,7 @@ function variablesV(){
     document.write( '<br>' );
     document.write( casado );
 };
+
 function entradaTeclado(){
     var nombre;
     var edad;
@@ -37,6 +38,7 @@ function entradaTeclado(){
     document.write(edad);
     document.write(' años');
 }
+
 function suma(){
     var valor1; 
     var valor2;
@@ -49,7 +51,9 @@ function suma(){
     document.write('<br>');
     document.write('El producto es ');
     document.write(producto);
+
 };
+
 function ifsentence(){
     var nombre;
     var nota;
@@ -59,3 +63,38 @@ function ifsentence(){
         document.write(nombre + ' está aprobado con un ' + nota);
     }
 };
+
+function ifElseSentence(){
+    var num1, num2;
+    num1 = prompt('Ingresa el primer número:', '');
+    num2 = prompt('Ingresa el segundo número:', '');
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    if (num1 > num2) {
+        document.write('el mayor es ' + num1);
+    } else {
+        document.write('el mayor es ' + num2);
+    }
+};
+
+function calificacion(){
+    var nota1, nota2, nota3;
+    nota1 = prompt('Ingresa 1ra. nota:', '');
+    nota2 = prompt('Ingresa 2da. nota:', '');
+    nota3 = prompt('Ingresa 3ra. nota:', '');
+    // Convertimos los 3 string en enteros
+    nota1 = parseInt(nota1);
+    nota2 = parseInt(nota2);
+    nota3 = parseInt(nota3);
+    var pro;
+    pro = (nota1 + nota2 + nota3) / 3;
+    if (pro >= 7) {
+        document.write('aprobado');
+    } else {
+        if (pro >= 4) {
+            document.write('regular');
+        } else {
+            document.write('reprobado');
+        }
+    }
+}
