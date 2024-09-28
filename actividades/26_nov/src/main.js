577016c897785cd4e33b13a407f54b17a783f137
Maurice <mauricio.gonzalezt@alumno.buap.mx>
2024-09-27 19:51:18 -0600

    ejercicio 3

--- actividades/26_nov/src/main.js
+++ actividades/26_nov/src/main.js
@@ -1,11 +1,23 @@
+function getDatos(){
+    var nombre = window.prompt("Nombre: ", " ");
+    var edad = prompt("Edad: ", " ");
+    var div1 = document.getElementById('nombre');
+    div1.innerHTML = '<h3> Nombre: ' + nombre + '</h3>';
+
+    var div2 = document.getElementById('edad');
+    div2.innerHTML = '<h3> Edad: ' + edad + '</h3>';
+};
+
 function helloWorld(){
     document.write("Hola mundo");
 };
+
 function variablesV(){
     var nombre = 'Juan';
     var edad = 10;
     var altura = 1.92;
     var casado = false;
+
     document.write( nombre );
     document.write( '<br>' );
     document.write( edad );
@@ -13,4 +25,28 @@ function variablesV(){
     document.write( altura );
     document.write( '<br>' );
     document.write( casado );
+};
+function entradaTeclado(){
+    var nombre;
+    var edad;
+    nombre = prompt('Ingresa tu nombre:', '');
+    edad = prompt('Ingresa tu edad:', '');
+    document.write('Hola ');
+    document.write(nombre);
+    document.write(' así que tienes ');
+    document.write(edad);
+    document.write(' años');
+}
+function suma(){
+    var valor1; 
+    var valor2;
+    valor1 = prompt('Introducir primer número:', '');
+    valor2 = prompt('Introducir segundo número:', '');
+    var suma = parseInt(valor1) + parseInt(valor2);
+    var producto = parseInt(valor1) * parseInt(valor2);
+    document.write('La suma es ');
+    document.write(suma);
+    document.write('<br>');
+    document.write('El producto es ');
+    document.write(producto);
 };
