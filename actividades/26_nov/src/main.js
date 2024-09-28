212f8ece6d0e490d70d4535d023a205f823cc38d
Maurice <mauricio.gonzalezt@alumno.buap.mx>
2024-09-27 20:21:28 -0600

    añadiendo ejercicio 15 y 16

--- actividades/26_nov/src/main.js
+++ actividades/26_nov/src/main.js
@@ -214,3 +214,31 @@ function corr(){
     document.write("Cuidado<br>");
     document.write("Ingresa tu documento correctamente<br>");
 };
+
+
+function mostrarMensaje() {
+    document.write("Cuidado<br>");
+    document.write("Ingresa tu documento correctamente<br>");
+};
+
+function corrV2(){    
+    mostrarMensaje();
+    mostrarMensaje();
+    mostrarMensaje();    
+};
+
+function mostrarRango(x1, x2) {
+    var inicio;
+    for (inicio = x1; inicio <= x2; inicio++) {
+        document.write(inicio + ' ');
+    }
+}
+
+function rang(){
+    var valor1, valor2;
+    valor1 = prompt('Ingresa el valor inferior:', '');
+    valor1 = parseInt(valor1);
+    valor2 = prompt('Ingresa el valor superior:', '');
+    valor2 = parseInt(valor2);
+    mostrarRango(valor1, valor2);    
+}
